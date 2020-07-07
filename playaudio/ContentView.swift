f48b99f1496812a5c00aa44af669b69b45882e09
kangyu <kingkangyu@163.com>
2020-07-07 20:29:11 +0800

    init

---
+++ playaudio/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  playaudio
+//
+//  Created by kangyu on 2020/7/7.
+//  Copyright © 2020 kangyu. All rights reserved.
+//
+
+import SwiftUI
+import AVFoundation
+import Combine
+
+struct ContentView: View {
+    @State var player:AVAudioPlayer?
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                //本机沙盒目录
+    //            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    //            let documentsDirectory = paths[0]
+    //
+    //            let filePath = URL(fileURLWithPath: "\(documentsDirectory)/example.mp3")
+                //bundle文件
+                let path = Bundle.main.path(forResource: "example.mp3", ofType:nil)!
+                let filePath = URL(fileURLWithPath: path)
+                
+                do {
+                    self.player = try AVAudioPlayer(contentsOf: filePath)
+                    if (player == nil) {
+                        print("player is nil")
+                    }
+                    self.player?.play()
+            
+                } catch let err {
+                    print("Failed:\(err.localizedDescription)")
+                }
+            }) {
+                Text("Play")
+            }.padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                self.player?.stop()
+            }) {
+                Text("Stop")
+            }.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
